803dd11c327850fe480a549760c701799c915a82
[jotarredondo] <jotarredondomail.com>
2022-07-28 10:23:43 -0400

    primer commit

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -3,8 +3,6 @@ import {ButtonModule} from 'primeng/button';
 import {CardModule} from 'primeng/card';
 import {MenubarModule} from 'primeng/menubar';
 
-
-
 @NgModule({
   declarations: [],
   imports: [
